f3a375cc88e78d1fb700742473032615909abbc8
MounaBnz <mouna.benzaied@insat.ucar.tn>
2022-01-16 00:18:40 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import './App.css';
+import axios from 'axios';
+
+class App extends React.Component {
+    state = {
+        advice : ''
+    };
+    componentDidMount(){
+        this.fetchAdvice();
+    }
+fetchAdvice =() => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then((response) => {
+        const{ advice } = response.data.slip;
+        console.log(advice);
+        this.setState({advice});
+
+    })
+    .catch((error) => {
+        console.log(error);
+
+    });
+}
+
+    render(){
+        const {advice} = this.state;
+        return(
+            <div className ='app'>
+          <div className ='card'>
+               <h1 className="heading">{advice}</h1>
+               <button className="button" onClick={this.fetchAdvice}>
+                   <span>
+                       Give Me Another One!
+                   </span>
+               </button>
+          </div>
+            </div>
+        );
+    }
+}
+ export default App; 
